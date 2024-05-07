ceb67ce551e2becfb51e58a1c103756220165321
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-07 13:06:34 -0300

    Navegação entre páginas 02

--- 03_basico/src/pages/rotas/index.jsx
+++ 03_basico/src/pages/rotas/index.jsx
@@ -1,6 +1,21 @@
 import Link from "next/link";
+import router, { useRouter } from "next/router";
 
 export default function Rotas(){
+	function navegacaoSimples(url){
+		router.push(url);
+	}
+
+	function navegacaoComParams(){
+		router.push({
+			pathname: "/rotas/params",
+			query: {
+				id: 123,
+				nome: "Ana Silva"
+			}
+		})
+	}
+
 	return (
 		<div>
 			<h1>Rotas Index</h1>
@@ -10,9 +25,12 @@ export default function Rotas(){
 				</Link>
 				<Link href="/rotas/123/buscar"><li>Buscar</li></Link>
 				<Link href="/rotas/123/Daniel"><li>Daniel</li></Link>
-				{/* <Link><li></li></Link> */}
-				{/* <Link><li></li></Link> */}
 			</ul>
+			<div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+				<button onClick={() => router.push("/rotas/123/buscar")}>Buscar</button>
+				<button onClick={() => navegacaoSimples("/rotas/123/Daniel")}>Daniel</button>
+				<button onClick={navegacaoComParams}>Ana</button>
+			</div>
 		</div>
 	)
 }
