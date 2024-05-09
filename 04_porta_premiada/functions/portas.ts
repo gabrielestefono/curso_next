c7d1276571bf842452344902574a8c329a182a2e
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-09 17:17:47 -0300

    Eventos da porta 03

---
+++ 04_porta_premiada/functions/portas.ts
@@ -0,0 +1,22 @@
+import PortaModel from "@/model/porta-model";
+
+export function criarPortas(quantidade: number, selecionada: number): PortaModel[]
+{
+	return Array.from({length: quantidade}, (_, i) => {
+		const numero = i +1;
+		const temPresente = numero === selecionada;
+		return new PortaModel(numero, temPresente);
+	});
+}
+
+export function atualizarPortas(portas: PortaModel[], modificada: PortaModel): PortaModel[]
+{
+	return portas.map(portaAtual => {
+		const igualModificada = portaAtual.numero === modificada.numero;
+		if(igualModificada){
+			return modificada;
+		}else{
+			return modificada.aberta ? portaAtual : portaAtual.desselecionar();
+		}
+	})
+}
