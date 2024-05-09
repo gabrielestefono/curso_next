1c319bfca8ca4a6f4c21b42779bb57bbaa2ac8fa
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-09 17:18:16 -0300

    Lidando com o array de portas 01

--- 04_porta_premiada/components/Porta.tsx
+++ 04_porta_premiada/components/Porta.tsx
@@ -2,28 +2,39 @@ import PortaModel from '@/model/porta-model';
 import styles from '../styles/Porta.module.css';
 import { FormEvent } from 'react';
 
-interface PortaProps{
+interface PortaProps {
 	value: PortaModel,
 	onChange: (novaPorta: PortaModel) => void
 }
 
-export default function Porta(props: Readonly<PortaProps>){
+export default function Porta(props: Readonly<PortaProps>) {
 	const porta = props.value;
-	const selecionada = porta.selecionada;
+	const selecionada = porta.selecionada && !porta.aberta;
 
-	function alternarSelecao(e: FormEvent){
+	function alternarSelecao(e: FormEvent) {
 		props.onChange(porta.alternarSelecao());
 	}
 
-	return(
-		<button className={styles.area} onClick={alternarSelecao}>
+	function abrir(e: FormEvent) {
+		e.stopPropagation();
+		props.onChange(porta.abrir());
+	}
+
+	function renderizarPorta() {
+		return (
+			<div className={styles.porta}>
+				<div className={styles.numero}>{porta.numero}</div>
+				<div className={styles.macaneta} onClick={abrir}></div>
+			</div>
+		)
+	}
+
+	return (
+		<div className={styles.area} onClick={alternarSelecao}>
 			<div className={`${styles.estrutura} ${selecionada ? styles.selecionada : ''}`}>
-				<div className={styles.porta}>
-					<div className={styles.numero}>{porta.numero}</div>
-					<div className={styles.macaneta}></div>
-				</div>
+				{porta.aberta ? false : renderizarPorta()}
 			</div>
 			<div className={styles.chao}></div>
-		</button>
+		</div>
 	)
 }
--- 04_porta_premiada/pages/index.tsx
+++ 04_porta_premiada/pages/index.tsx
@@ -1,14 +1,18 @@
 import Porta from "@/components/Porta";
-import PortaModel from "@/model/porta-model";
+import { criarPortas, atualizarPortas } from "@/functions/portas";
 import { useState } from "react";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1));
-  const [texto, setTexto] = useState("...");
+  const [portas, setPortas] = useState(criarPortas(20, 5));
+
+  function renderizarPortas() { 
+    return portas.map((porta) => 
+      <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
+  ) }
+
   return (
-    <div style={{display: "flex", flexDirection: "column"}}>
-      <input type="text" value={texto} onChange={e => setTexto(e.target.value)}/>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+    <div style={{ display: "flex", flexShrink: "0", flexWrap: "wrap"}}>
+      {renderizarPortas()}
     </div>
   );
 }
